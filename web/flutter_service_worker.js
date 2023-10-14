e0013af348495d30849c7f6c12eaeebf7c096bbb
rymin <rymin@clllin.com>
2023-10-14 17:20:41 +0900

    asdf

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "73be94451399d6d89ceb7499d04660f1",
-"index.html": "1528abc9f58133e84aba0adee304aa1f",
-"/": "1528abc9f58133e84aba0adee304aa1f",
-"main.dart.js": "4a912a928654cef83aed892d480558b4",
+"index.html": "a3b8e30a749a377db182948733d74821",
+"/": "a3b8e30a749a377db182948733d74821",
+"main.dart.js": "72c1b5daf7c2eec64c4449ed701c9cc4",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
